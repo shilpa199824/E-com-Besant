3fb4ed7cbbd4a93923518d9276cb06d8e3488711
shilpa199824 <shiplabvr1998@gmail.com>
2025-01-08 22:52:51 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Cart2 from './Components/Cart2';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Samsung from './Components/Phones1/Samsung';
+import Iphone from './Components/Phones1/Iphone';
 
 
 
@@ -11,8 +12,7 @@ import Samsung from './Components/Phones1/Samsung';
 function App() {
     return (
         <div>
-
-            <Samsung />
+            <Iphone />
 
 
 
