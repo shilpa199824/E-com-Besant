aff795f11ab3e5e2ed91365f07c332db1772d250
srustimh0 <srustimh05@gmail.com>
2025-01-08 23:27:35 +0530

    app.js modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Cart2 from './Components/Cart2';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Samsung from './Components/Phones1/Samsung';
-import Iphone from './Components/Phones1/Iphone';
+// import Iphone from './Components/Phones1/Iphone';
 
 
 
@@ -12,7 +12,7 @@ import Iphone from './Components/Phones1/Iphone';
 function App() {
     return (
         <div>
-            <Iphone />
+            <Samsung />
 
 
 
